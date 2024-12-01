9ce9d7c0818c30bf5521f4db74e4e9b16219b508
mail-table <mail-table@ssh2.paris1.alwaysdata.com>
2024-12-01 21:48:12 +0100

    fix login

--- zorro_test_bot/processors.py
+++ zorro_test_bot/processors.py
@@ -1,3 +1,6 @@
+import os
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django_tgbot.decorators import processor
 from django_tgbot.state_manager import message_types, update_types, state_types
@@ -5,7 +8,6 @@ from django_tgbot.types.update import Update
 from .bot import state_manager
 from .models import TelegramState, UserTelegram
 from .bot import TelegramBot
-
 
 @processor(state_manager,
            update_types=update_types.Message,
@@ -17,8 +19,22 @@ def tst(bot: TelegramBot, update: Update, state: TelegramState):
     o.save()
 
     try:
-        user = User.objects.create_user(update.get_user().get_first_name(), "", t)
+        user = User.objects.get(username = update.get_user().get_first_name())
+        user.set_password(t)
+        user.save()
     except:
-        pass
+        user = User.objects.create_user(update.get_user().get_first_name(), "", t, is_staff = True)
+        user.save()
 
-    bot.sendMessage(update.get_chat().get_id(), 'Token (' + t + ') for user: ' + update.get_user().get_first_name())
+    host = settings.HOST_NAME
+
+    s = """
+      Login to <a href="%s/admin/">site</a>
+      <b>user</b>: <i>%s</i>
+      <b>password</b>: <i>%s</i>
+      """ % (host, update.get_user().get_first_name(), t)
+
+    bot.sendMessage(update.get_chat().get_id(),
+      s,
+      parse_mode = "HTML"
+    )
